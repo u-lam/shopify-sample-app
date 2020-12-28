084ed937265db3d130b97be5d268533c96a07802
uyen <ulam10@gmail.com>
2020-12-28 14:55:03 -0800

    init commit with dependencies and basic node server setup

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <p>Sample app using React and Next.js</p>
+    </div>
+  );
+};
+
+export default Index;
---
+++ server.js
@@ -0,0 +1,47 @@
+// setting up node server with dependencies
+require("isomorphic-fetch");
+const dotenv = require("dotenv");
+const Koa = require("koa");
+const next = require("next");
+const { default: createShopitfyAuth } = require("@shopify/koa-shopify-auth");
+const { verifyRequest } = require("@shopify/koa-shopify-auth");
+const session = require("koa-session");
+
+dotenv.config();
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+
+// adding routing middleware and koa server
+app.prepare().then(() => {
+  const server = new Koa();
+  server.use(session({ secure: true, sameSite: "none" }, server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
+  server.use(
+    // The createShopifyAuth functions take the Shopify API key and the Shopify API secret key and trigger the authentication screen. The function provides you the afterAuth, where you can use your own logic or redirect.
+    createShopitfyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ["read_products"],
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+        ctx.redirect("/");
+      }
+    })
+  );
+  // The verifyRequest redirects users to the OAuth route if they haven’t been authenticated.
+  server.use(verifyRequest());
+  server.use(async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+    return;
+  });
+
+  // setting port to listen on 3000
+  server.listen(port, () => console.log(`>Ready on http:localhost:${port}`));
+});
